8e378836858c2b4ccd49f23555d21edfbdfa4a76
Scott Cunningham <cunninsc@tcd.ie>
2013-10-27 18:41:47 +0000

    Fixed DB handling things

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def query_db(query, args=()):
 def get_db():
     db = getattr(g, '_database', None)
     if db is None:
-        db = g._database = sqlite3.connect(DATABASE)
+        db = sqlite3.connect(DATABASE)
     return db
 
 @app.before_request
@@ -27,12 +27,6 @@ def before_request():
 @app.teardown_request
 def teardown_request(exception):
     db = getattr(g, 'db', None)
-    if db is not None:
-        db.close()
-
-@app.teardown_appcontext
-def close_connection(exception):
-    db = getattr(g, '_database', None)
     if db is not None:
         db.close()
 
