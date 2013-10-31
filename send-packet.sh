2295c7c389971a2b4249740be2e98f5478f32a8c
Scott Cunningham <cunninsc@tcd.ie>
2013-10-31 23:13:14 +0000

    added tool to send fake waspmote packets

--- send-packet.sh
+++ send-packet.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-printf "POST /upload HTTP/1.1\r\nContent-Type: application/sdadasdax-www-form-urlencoded\r\nuid=123\%26x=234\%26z=345" | nc localhost 5000
+curl -d "uid=123&x=345&z=456" http://localhost:5000/upload
